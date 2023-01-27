763098dc7bd1fbbb155d5515ac94fa2470a77d9f
Dinesh <dk1812001@gmail.com>
2023-01-27 16:24:20 +0530

    2nd commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JiraBoardComponent } from './component/jira-board/jira-board.component';
+import { TimeOrganiserComponent } from './component/time-organiser/time-organiser.component';
+import { TimesheetComponent } from './component/timesheet/timesheet.component';
+import { ToDoComponent } from './component/to-do/to-do.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'timesheet', component: TimesheetComponent},
+  {path:'time-org', component: TimeOrganiserComponent},
+  {path:'to-do', component: ToDoComponent},
+  {path:'jira', component: JiraBoardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
